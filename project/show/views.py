cbd393567f03d5ecbfd473e39513dde27d6dcfe5
imadeddinedjekoune <imaddjekoune@gmail.com>
2023-09-04 22:35:25 -0700

    second

--- project/show/views.py
+++ project/show/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Person 
+
 
 def index(request):
-	return HttpResponse("Hello World")
+
+	return HttpResponse(Person.objects.all())
